1690f1effe77cccf390d56e0643507be260b90a2
Orest Sonich <sonich.work@gmail.com>
2023-07-31 02:10:42 +0300

    HTTP status code "ok" replaced with new, more appropriate code - "created", in cases where service create new Entity in DAO

--- src/main/java/com/orest/app/fresheng/exceptions/EmailAreBusyException.java
+++ src/main/java/com/orest/app/fresheng/exceptions/EmailAreBusyException.java
@@ -5,6 +5,5 @@ import org.springframework.security.core.AuthenticationException;
 public class EmailAreBusyException extends AuthenticationException {
     public EmailAreBusyException(String msg) {
         super(msg);
-        System.err.println("Email are busy");
     }
 }
